feed62d7d92846dc83433f2803244aabb48aa52f
Brian Picciano <mediocregopher@gmail.com>
2018-05-27 07:57:23 +0000

    mtime: fix older package doc not having been deleted

--- mtime/mtime.go
+++ mtime/mtime.go
@@ -1,5 +1,2 @@
-// Package mtime provides a wrapper around time.Time which marshals and unmarshals
-// it as a floating point unix timestamp.
-
 // Package mtime extends the standard time package with extra functionality
 package mtime
